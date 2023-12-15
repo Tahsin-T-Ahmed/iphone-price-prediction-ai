88b6433bcf708e622ca44b594f327a46456176db
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2023-12-14 23:59:41 -0500

    Added comments to code

--- psql/exporter.py
+++ psql/exporter.py
@@ -7,6 +7,7 @@ pw_filepath = os.path.join(os.getcwd(), "password.txt")
 
 dbpassword = open(pw_filepath, 'r').read()
 
+# CONNECTING TO POSTGRES DATABASE
 conn = psycopg2.connect(
     host = "localhost",
     dbname = "iphone_prices",
@@ -17,16 +18,23 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
+# GETTING RECORDS FROM DATABASE 
+
 cur.execute("SELECT * FROM models;")
 
+# getting field names 
 fields = [field[0] for field in cur.description]
 
+# creating empty dataframe with columns as field names
 iphones = pd.DataFrame(columns=fields)
 
+# populating dataframe with database records 
 for record in cur.fetchall():
     iphones.loc[len(iphones)] = record
 
+# closing connection to database
 cur.close()
 conn.close()
 
+# EXPORTING DATAFRAME AS CSV FILE 
 iphones.to_csv("./iphone_releases.csv", index=False)
