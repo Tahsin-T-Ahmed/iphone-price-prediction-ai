69b45700bb3c0c13d3b738ddee1b11828c270e43
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2023-12-14 22:48:55 -0500

    basal code for psql connection

--- psql/exporter.py
+++ psql/exporter.py
@@ -1,3 +1,24 @@
 import psycopg2
 import os 
 
+# READING PASSWORD FROM HIDDEN FILE: password.txt
+pw_filepath = os.path.join(os.getcwd(), "password.txt")
+
+dbpassword = open(pw_filepath, 'r').read()
+
+conn = psycopg2.connect(
+    host = "localhost",
+    dbname = "postgres",
+    user = "postgres",
+    password = dbpassword,
+    port = 5432
+)
+
+cur = conn.cursor()
+
+
+
+conn.commit()
+
+cur.close()
+conn.close()
