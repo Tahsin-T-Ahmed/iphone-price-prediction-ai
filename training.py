a1c5e5422f4e10485809f24ba7789c7fd9be6ebc
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-17 23:21:46 -0400

    standardized numerical features of dataset

--- training.py
+++ training.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import joblib
+from sklearn.preprocessing import StandardScaler
 
 data_path = "./iphones_linreg_output.csv"
 
@@ -14,4 +15,11 @@ storage_dummies = [gb for gb in iphones.columns if "GB" in gb]
 
 numerical_features = ["YEAR"]
 binary_features = ["SPECIAL", "LARGE", *storage_dummies]
+
+X = iphones[numerical_features + binary_features]
+y = iphones["PRICE"]
+
+scaler = StandardScaler()
+scaler.fit(X[numerical_features])
+X[numerical_features] = scaler.fit_transform(X[numerical_features])
 
