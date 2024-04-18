ca4d1ae46097a3c2a8014d2083ce7c3a35acd2b6
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-17 23:07:49 -0400

    prepare dataset for training

--- training.py
+++ training.py
@@ -7,3 +7,11 @@ data_path = "./iphones_linreg_output.csv"
 
 iphones = pd.read_csv(data_path)
 
+#drop unnamed column
+iphones = iphones.iloc[:, 1:]
+
+storage_dummies = [gb for gb in iphones.columns if "GB" in gb]
+
+numerical_features = ["YEAR"]
+binary_features = ["SPECIAL", "LARGE", *storage_dummies]
+
