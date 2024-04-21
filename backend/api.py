da47b2c0473bfdcd18268407647aa28dd6636b10
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-21 03:54:46 -0400

    started function for default route

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def serve_pred():
+
+    return "Placeholder"
+
+if "__main__" == __name__:
+    app.run(debug=True)
