d12a8948e867714d3d1b21b49934fe548eaa257b
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-21 04:25:24 -0400

    set method to 'POST'; return input data (placeholder)

--- backend/api.py
+++ backend/api.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import numpy as np
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def serve_pred():
     df = pd.DataFrame(columns=["YEAR", "SPECIAL", "LARGE", *[f"GB_{2**i}" for i in range(6, 10+1)]])
 
-    return "Placeholder"
+    return request.json
 
 if "__main__" == __name__:
     app.run(debug=True)
