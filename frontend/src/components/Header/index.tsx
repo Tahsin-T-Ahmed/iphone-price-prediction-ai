882ab90d2b7c289ba79cc48bf669af40bdf8ebd6
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-26 21:44:13 -0400

    spaced between header and main

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,7 +3,9 @@ import { BsApple } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <header className={styles.name}>
+    <header className={styles.name} style={{
+      marginBottom: "2rem"
+    }}>
       <h1><BsApple className={styles.logo}/>iPhone4Cast AI</h1>
       <p>Predicting iPhone prices with Machine Learning</p>
     </header>
