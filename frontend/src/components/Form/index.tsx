45c609b7bb7db84255aa94d296c1675a8f610769
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-27 14:55:47 -0400

    changed POST request to /pred route

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -24,7 +24,7 @@ const Form = ({setFormData}) => {
             memory: evt.target["memory-item"].value
         }
 
-        const data = await fetch("http://127.0.0.1:5000/", {
+        const data = await fetch("http://127.0.0.1:5000/pred", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
