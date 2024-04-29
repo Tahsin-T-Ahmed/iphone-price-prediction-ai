ad25eb95f4d92efcf61c24c662759890a9e7335e
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2024-04-28 20:53:47 -0400

    setup graph API call to /graph

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -43,7 +43,19 @@ const Form = ({setFormData}) => {
 
         setFormData(formData)
 
-        // alert(result)
+        const graph = await fetch("http://127.0.0.1:5000/graph", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }).then(res => res.text())
+
+        formData.graph = graph
+
+        setFormData(formData)
+
+        console.log(formData)
     }
 
     const currentDate = new Date();
